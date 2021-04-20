e3fe3946a634dec6b75a0bf77bf111fbc49bd471
psevdaprogrammist <korchagin.egorka1@gmail.com>
2021-04-20 16:29:41 +1000

    Initial Commit

---
+++ SpotifyDesign/SpotifyDesignApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpotifyDesignApp.swift
+//  SpotifyDesign
+//
+//  Created by Egor Korchagin on 20.04.2021.
+//
+
+import SwiftUI
+
+@main
+struct SpotifyDesignApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
