37373d0b39bed55e836aa32e809848ff302fe055
psevdaprogrammist <korchagin.egorka1@gmail.com>
2021-04-21 14:27:51 +1000

    Search
    Recently played album cover + button Play

--- Model/Song.swift
+++ Model/Song.swift
@@ -20,14 +20,14 @@ var recentlyPlayed = [
     
     Song(album_name: "Ye", album_author: "Kanye West", album_cover: "Ye"),
     Song(album_name: "Kanye", album_author: "Kaney West", album_cover: "kanye"),
-    Song(album_name: "Man on the Moon", album_author: "Kid Cudi", album_cover: "kidcudi"),
-    Song(album_name: "Jesus is king", album_author: "Kanye West", album_cover: "jesus")
+    Song(album_name: "Man on the Moon", album_author: "Kid Cudi", album_cover: "kidcudi")
 ]
 
 var likedSongs = [
     Song(album_name: "Любимые песни", album_author: "Husky", album_cover: "h1"),
     Song(album_name: "Иуда", album_author: "Husky", album_cover: "h2"),
-    Song(album_name: "Хошхоног", album_author: "Husky", album_cover: "h3")
+    Song(album_name: "Хошхоног", album_author: "Husky", album_cover: "h3"),
+    Song(album_name: "Jesus is king", album_author: "Kanye West", album_cover: "jesus")
 ]
 
 var generes = ["Rap","Hip-Hop","Pop"]
--- View/HomeView.swift
+++ View/HomeView.swift
@@ -8,95 +8,94 @@
 import SwiftUI
 
 struct HomeView: View {
-    // Storing Current Tab
-    @State var selectedTab = "house.fill"
     
-    // Volume
-    @State var volume: CGFloat = 0.4
+    // Search Text
+    @State var searchText = ""
+
     var body: some View {
         HStack(spacing: 0) {
             
-            let lightWhite = Color.white.opacity(0.7)
-            // Side Tab Bar
-            VStack {
-                
-                Image("spotify")
-                    .resizable()
-                    .aspectRatio(contentMode: .fill)
-                    .frame(width: 45, height: 45)
-                    .padding(.top)
-                
-                VStack {
-                    
-                    TabButton(selectedTab: $selectedTab, image: "house.fill")
-                    
-                    TabButton(selectedTab: $selectedTab, image: "safari.fill")
-                    
-                    TabButton(selectedTab: $selectedTab, image: "mic.fill")
-                    
-                    TabButton(selectedTab: $selectedTab, image: "clock.fill")
-                }
-                .frame(height: getRect().height / 2.3)
-                .padding(.top)
-                
-                Spacer(minLength: 50)
-                
-                Button(action: {
-                    // Checking and increasing volume
-                    volume = volume + 0.1 < 1.0 ? volume + 0.1 : 1
-                }, label: {
-                    Image(systemName: "speaker.wave.2.fill")
-                        .font(.title2)
-                        .foregroundColor(.white)
-                })
-                
-                // Custom VolumeProgressView
-                
-                GeometryReader {proxy in
-                
-                    //Extrasing progress bar height and based on that getting progress value
-                    let height = proxy.frame(in: .global).height
-                    let progress = height * volume
-                
-                    ZStack(alignment: .bottom) {
-                        
-                        Capsule()
-                            .fill(Color.gray.opacity(0.5))
-                            .frame(width: 4)
-                        
-                        Capsule()
-                            .fill(Color.white)
-                            .frame(width: 4, height: progress)
-                    }
-                    .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center)
-                    
-                }
-                .padding(.vertical, 20)
-                
-                Button(action: {// Checking and decreasing volume
-                        volume = volume - 0.1 > 0 ? volume - 0.1 : 0}, label: {
-                    Image(systemName: "speaker.wave.1.fill")
-                        .font(.title2)
-                        .foregroundColor(.white)
-                })
-                
-                Button(action: {}, label: {
-                    Image(systemName: "chevron.left")
-                        .font(.title2)
-                        .foregroundColor(lightWhite)
-                })
-                .padding(.top, 30)
-                .padding(.bottom, getSafeArea().bottom == 0 ? 15: 0)
-            }
-            .frame(width: 80)
-            .background(Color.black.ignoresSafeArea())
+            SideTabView()
             
             //Main Content
             ScrollView(showsIndicators: false, content: {
                 
+                VStack(spacing: 15) {
+                    
+                    HStack(spacing: 15) {
+                        
+                        HStack(spacing: 15) {
+                            
+                            
+                            
+                            
+                            Circle()
+                                .stroke(Color.white, lineWidth: 4)
+                                .frame(width: 25, height: 25)
+                            TextField("Search...", text: $searchText)
+                            
+                        }
+                        .padding(.vertical, 10)
+                        .padding(.horizontal)
+                        .background(Color.white.opacity(0.08))
+                        .cornerRadius(8)
+                        
+                        Button(action: {}, label: {
+                            Image("profile")
+                                .resizable()
+                                .aspectRatio(contentMode: .fill)
+                                .frame(width: 45, height: 45)
+                                .cornerRadius(10)
+                        })
+                    }
+                    
+                    Text("Recently Played")
+                        .font(.title)
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .padding(.top, 30)
+                    
+                    // Carousel list
+                    TabView {
+                        
+                        ForEach(recentlyPlayed) {item in
+                            
+                            ZStack(alignment:.bottomLeading) {
+                                Image(item.album_cover)
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fill)
+                                    .cornerRadius(20)
+                                    
+                                
+                                HStack {
+                                    
+                                    Button(action: {}, label: {
+                                        Image(systemName: "play.fill")
+                                            .font(.title2)
+                                            .foregroundColor(.white)
+                                            .padding(20)
+                                            .background(Color("logoColor"))
+                                            .clipShape(Circle())
+                                    })
+                                }
+                                .padding()
+                            }
+                            .padding(.horizontal)
+                            
+                        }
+                    }
+                    // Max Frame
+                    .frame(height: 350)
+                    .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
+                    .padding(.top, 20)
+                }
+                .padding()
+                .frame(maxWidth: .infinity)
             })
-            .background(Color("bg").ignoresSafeArea())
+            
         }
+        .background(Color("bg").ignoresSafeArea())
     }
 }
 
@@ -106,31 +105,4 @@ struct HomeView_Previews: PreviewProvider {
     }
 }
 
-struct TabButton: View {
-    
-    @Binding var selectedTab: String
-    var  image : String
-    
-    var body: some View {
-        
-        Button(action: {withAnimation{selectedTab = image}}, label: {
-            Image(systemName: image)
-                .font(.title)
-                .foregroundColor(selectedTab == image ? .white : Color.gray.opacity(0.6))
-                .frame(maxHeight: .infinity)
-        })
-    }
-    
-}
 
-// Extending View to get Screen Size
-
-extension View {
-    func getRect()->CGRect {
-        return UIScreen.main.bounds
-    }
-    // Getting SafeArea
-    func getSafeArea() -> UIEdgeInsets {
-        return UIApplication.shared.windows.first?.safeAreaInsets ?? UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
-    }
-}
